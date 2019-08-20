ebe245cd3503ee582d2d3b13672b63bd8c3c602d
hkantare <hkantare@in.ibm.com>
2019-08-20 19:48:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,3 +6,10 @@ resource "ibm_is_vpc" "vpc" {
   name           = "${var.vpc_name}"
   resource_group = "${data.ibm_resource_group.group.id}"
 }
+
+resource "null_resource" "test" {
+   provisioner "local-exec" {
+    command = "echo hello"
+     }
+  
+  }
