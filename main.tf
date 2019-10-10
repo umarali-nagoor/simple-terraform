4e589d0cbf498b3a9aeb5d600dceae8d5ee2c98f
hkantare <hkantare@in.ibm.com>
2019-10-10 16:02:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,10 +2,10 @@ data "ibm_resource_group" "group" {
   name = "${var.resource_group_name}"
 }
 
-resource "ibm_is_vpc" "vpc" {
+/*resource "ibm_is_vpc" "vpc" {
   name           = "${var.vpc_name}"
   resource_group = "${data.ibm_resource_group.group.id}"
-}
+}*/
 
 resource "null_resource" "test" {
    provisioner "local-exec" {
