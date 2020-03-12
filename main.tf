82e6e7acacc2616e56f810f13b05ec2d2272b418
hkantare <hkantare@in.ibm.com>
2020-03-12 15:43:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,6 +5,7 @@ data "ibm_resource_group" "group" {
 resource "ibm_is_vpc" "vpc" {
   name           = "${var.vpc_name}"
   resource_group = "${data.ibm_resource_group.group.id}"
+  tags = ["tag1","tag2"]
 }
 
 /*resource "null_resource" "test" {
