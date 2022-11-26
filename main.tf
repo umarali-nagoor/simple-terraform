76674aa2de0f70fa66c3d2745441c8ff6de04cb3
HARINI KANTAREDDY <hkantare@in.ibm.com>
2022-11-26 09:23:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-data "ibm_resource_group" "group" {
+/*data "ibm_resource_group" "group" {
   name = "${var.resource_group_name}"
 }
 
@@ -22,7 +22,7 @@ resource "ibm_iam_user_invite" "stss" {
   users         = local.email_stss
   access_groups = [ibm_iam_access_group.stss.id]
   lifecycle {ignore_changes = [invited_users]}
-}*/
+}
 
 resource "ibm_iam_access_group" "stss" {
   name                               = "RN-STSS-1"
@@ -33,7 +33,7 @@ data "ibm_iam_access_group" "accgroup" {
   access_group_name = "VPC"
 }
 
-/*resource "null_resource" "test" {
+resource "null_resource" "test" {
    provisioner "local-exec" {
     command = "echo hello"
      }
