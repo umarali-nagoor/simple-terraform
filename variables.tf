060d3c9eb4fc89e24d95782dcdeaf25e4dd550a5
HARINI KANTAREDDY <hkantare@in.ibm.com>
2022-12-08 18:15:11 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -10,3 +10,15 @@ variable "resource_group_name" {
 variable "vpc_name" {
     default = "test"
 }
+
+output "schematics_workspace_id" {
+  description = "ID of the IBM Cloud Schematics workspace. Returns null if not ran in Schematics"
+  value       = var.IC_SCHEMATICS_WORKSPACE_ID
+}
+
+variable "IC_SCHEMATICS_WORKSPACE_ID" {
+  default     = ""
+  type        = string
+  description = "leave blank if running locally. This variable will be automatically populated if running from an IBM Cloud Schematics workspace"
+}
+
