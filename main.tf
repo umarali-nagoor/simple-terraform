b7fe2c06e59ff7e5724a0152250be8fadbe1236c
HARINI KANTAREDDY <hkantare@in.ibm.com>
2023-01-04 14:28:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,18 +3,17 @@ data "ibm_resource_group" "group" {
   name = "${var.resource_group_name}"
 }
 
-resource "ibm_cd_toolchain" "cd_toolchain_instance" {
+/*resource "ibm_cd_toolchain" "cd_toolchain_instance" {
   description = "A sample toolchain to test the API"
   name = "TestToolchainV2"
   resource_group_id = "${data.ibm_resource_group.group.id}"
-}
+}*/
 
-/*resource "ibm_is_vpc" "vpc" {
+resource "ibm_is_vpc" "vpc" {
   name           = "${var.vpc_name}"
   resource_group = "${data.ibm_resource_group.group.id}"
-  //default_security_group_name = "unseated-spoilage-surpass-prompt"
   tags = ["tag1","tag2"]
-}*/
+}
 
 
 /*resource "ibm_is_vpc" "vpc1" {
