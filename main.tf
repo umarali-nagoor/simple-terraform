9f6e957c00b714226ab0073ad9eb8e28562f8339
Umar Ali <umarali@Umars-MacBook-Pro.local>
2024-05-29 17:18:11 +0530

    Added VPC block

--- main.tf
+++ main.tf
@@ -2,3 +2,8 @@ data "ibm_resource_group" "group" {
   name = var.resource_group_name
 }
 
+resource "ibm_is_vpc" "vpc" {
+  name           = "${var.vpc_name}"
+  resource_group = "${data.ibm_resource_group.group.id}"
+  tags = ["tag1","tag2"]
+}
--- variables.tf
+++ variables.tf
@@ -17,6 +17,7 @@ variable "resource_group_name" {
 
 variable "vpc_name" {
     default = "test"
+    sensitive   = true
 }
 
 variable "IC_SCHEMATICS_WORKSPACE_ID" {
